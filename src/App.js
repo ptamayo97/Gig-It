bf878eae4db23925d91fbfd2f13dbb89b7d28326
Tim Westberg <Timwestberg@formulagen.com>
2019-08-08 20:55:37 -0700

    Added all initial component files, with starting code in each folder for easy quick start

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+// import { Switch, Route } from 'react-router-dom';
+// import { MDBCol, MDBRow, MDBContainer } from 'mdbreact';
+// import RoutesWithNavigation from './components/RoutesWithNavigation';
+// import Home from './pages/Home';
+import MapPage from './components/Map/index';
+// import Bands from './pages/Bands';
+// import EventRegister from './pages/RegisterEvent';
+// import Account from './pages/Account';
+// import BandProfile from './pages/BandProfile';
 
-import RoutesWithNavigation from './components/RoutesWithNavigation';
-import Home from './pages/Home';
-import MapPage from './pages/MapePage';
-import Bands from './pages/Bands';
-import EventRegister from './pages/RegisterEvent';
-import Account from './pages/Account';
-import BandProfile from './pages/BandProfile';
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<MapPage />
+			</div>
+		);
+	}
+}
 
-const App = () => {
-	return (
-		<Switch>
-			<Route path='/pages/Home' exact component={Home} />
-			<Route path='/pages/Map' exact component={MapPage} />
-			<Route path='/pages/Bands' exact component={Bands} />
-			<Route path='/pages/RegisterEvent' exact component={EventRegister} />
-			<Route path='/pages/Account' exact component={Account} />
-			<Route path='/pages/BandProfile' exact component={BandProfile} />
+// const App = () => {
+// 	return (
+// 		<Switch>
+// 			<Route path='/pages/Home' exact component={Home} />
+// 			<Route path='/pages/Map' exact component={MapPage} />
+// 			<Route path='/pages/Bands' exact component={Bands} />
+// 			<Route path='/pages/RegisterEvent' exact component={EventRegister} />
+// 			<Route path='/pages/Account' exact component={Account} />
+// 			<Route path='/pages/BandProfile' exact component={BandProfile} />
 
-			<RoutesWithNavigation />
-		</Switch>
-	);
-};
+// 			<RoutesWithNavigation />
+// 		</Switch>
+// 	);
+// };
 
 export default App;
