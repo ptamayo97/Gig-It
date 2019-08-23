65c99c85edec1b26bfba225369177698348f2a6e
ptamayo <ptamayo97@yahoo.com>
2019-08-22 18:00:18 -0700

    firebase files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import MapPage from './components/Map';
+const App = () => {
+	return (
+		<div>
+			<NavBar />
+			<MapPage />
+		</div>
+	);
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
-import App from './components/App';
-import reducers from './reducers';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig'
 
 // This is how a middleware is applied
-const store = createStore(reducers, applyMiddleware(thunk));
-
-ReactDom.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.querySelector('#root')
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(fbConfig, {userProfile: 'users', useFirestoreForProfile: true, attachAuthIsReady: true}),
+    reduxFirestore(fbConfig) // redux bindings for firestore
+  )
 );
+
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+  registerServiceWorker();
+});
