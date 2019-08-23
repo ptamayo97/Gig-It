9ac747ec39f325451681e6c5dd4dafef484aa13c
ptamayo <ptamayo97@yahoo.com>
2019-08-22 19:56:26 -0700

    package fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import MapPage from './components/Map';
+import Home from './pages/Home';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+// import EventProfile from '../components/Profiles/Event';
 const App = () => {
 	return (
-		<div>
-			<NavBar />
-			<MapPage />
-		</div>
+		<BrowserRouter>
+			<div className='App'>
+				<NavBar />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route path='/map' component={MapPage} />
+					<Route path='/signin' component={SignIn} />
+					<Route path='/signup' component={SignUp} />
+					{/* <Route path='/project/:id' component={EventProfile} /> */}
+				</Switch>
+			</div>
+		</BrowserRouter>
 	);
 };
 
